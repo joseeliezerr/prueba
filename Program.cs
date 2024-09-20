fc7ef26dd5e18b014902298cfd29df1a41ef1b7b
Jose Eliezer Rivera <Joseeliezerr123@gmail.com>
2024-09-19 20:20:38 -0600

    Actualizacion

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hola a todos!");
+
+int edad = 20;
+long altura = 165;
+double dinero = 1651.36;
+string nombre = "Eliezer";
+float danio = 124.5f;
+decimal monto = 4565.55m;
+char letra = 'A';
+bool tieneGorro = false;
+
+Console.WriteLine("Escribe tu nombre: ");
+nombre=Console.ReadLine();
+
+
+Console.WriteLine("Mi nombre es: "+nombre+" mi edad es: "+edad+"mi altura es: "+altura);
